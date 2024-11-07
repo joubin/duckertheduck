a9ae06e4e42582ef4ddde27cb1eb9288709614ad
Joubin Jabbari <joubin@jabbari.io>
2024-11-06 18:20:37 -0800

    Testing

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 apt install -y python3-pip python3 git
 cd /root
 git clone https://github.com/joubin/duckertheduck.git
+sleep 1
 cp /root/duckertheduck/sound_sensor.service /etc/systemd/system/sound_sensor.service
 cd /root/duckertheduck/db_sensor/
 python3 -m venv venv 
