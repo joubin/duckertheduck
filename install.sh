82f1d0cdb63f29b883ddb4f87665fce754818fb7
Joubin Jabbari <joubin@jabbari.io>
2024-11-06 17:52:20 -0800

    Testing

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
+apt install -y python3-pip python3 git
+cd root
+git clone https://github.com/joubin/duckertheduck.git
 
+cd duckertheduck/db_seonsor
+
+python3 -m venv venv 
+source venv/bin/activate
+pip3 install -r requirements.txt
+python3 main.py
